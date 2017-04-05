e3eed1cbb6bb82c4909008e2afbfc9889a4455b8
chomzi <arczi130@gmail.com>
2017-04-05 17:13:29 +0200

    changes

--- JavaEE/Przychodnia_lekarska/src/java/pl/goralczyk/controllers/PacjentBean.java
+++ JavaEE/Przychodnia_lekarska/src/java/pl/goralczyk/controllers/PacjentBean.java
@@ -1,25 +1,16 @@
 package pl.goralczyk.controllers;
 
-import com.mysql.jdbc.Statement;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import static java.util.Date.from;
 import java.util.List;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.hibernate.sql.Select;
 import pl.goralczyk.config.DBManager;
 import pl.goralczyk.config.DataConnect;
 import pl.goralczyk.config.SessionUtils;
